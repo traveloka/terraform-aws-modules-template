9de20d0e2965ae553f2c61eba7300299cab4b320
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2019-07-17 16:55:52 +0700

    add explanation again

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# This file contains terraform version constrains, provider definition, and also main resources.
+# https://www.terraform.io/docs/configuration/resources.html
+
 terraform {
   required_version = ">= 0.11.14, < 0.12.0"
 }
